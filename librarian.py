7b64b7f4868499d5855bff415704f40829dee4ac
James Milne <s4b3r6@gmail.com>
2015-12-06 19:49:40 +1100

    Conceptual Beginnings

--- librarian.py
+++ librarian.py
@@ -1,1 +1,16 @@
+import uuid
 
+class Book(object):
+    def __init__(self):
+        self.id = uuid.uuid4()
+        self.title = "Untitled"
+        self.author = ["Unknown"]
+        self.length = 0
+
+class Collection(object):
+    def __init__(self):
+        self.id = uuid.uuid4()
+        self.title = "Untitled"
+        self.author = ["Unknown"]
+        self.length = 0
+        self.count = 0
